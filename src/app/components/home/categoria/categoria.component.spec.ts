1acfeaff3df93339bbc074e951292c3235c35b82
Luiz Paulo Caldas <luiz21.lupa@gmail.com>
2024-07-08 15:24:53 -0300

    test: add tests in features

--- src/app/components/home/categoria/categoria.component.spec.ts
+++ src/app/components/home/categoria/categoria.component.spec.ts
@@ -1,29 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CategoriaComponent } from './categoria.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TableModule } from 'primeng/table';
-import { DialogModule } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
-import { RatingModule } from 'primeng/rating';
-import { CommonModule } from '@angular/common';
-import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { FormsModule, NgControl } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+import { HttpTestingController } from '@angular/common/http/testing';
+
+import { MessageService } from 'primeng/api';
+import { CategoriaComponent } from './categoria.component';
+import { CategoriaService } from '../../../services/categoria.service';
+import { ICategoria } from 'src/app/interfaces/iCategoria';
+import { Table } from 'primeng/table';
 
 describe('CategoriaComponent', () => {
   let component: CategoriaComponent;
   let fixture: ComponentFixture<CategoriaComponent>;
+  let categoriaService: CategoriaService;
+  let httpMock: HttpTestingController;
+  let messageService: MessageService;
+  let table: Table;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CategoriaComponent],
       imports: [HttpClientModule],
-      providers: [HttpClient, MessageService],
+      providers: [HttpClient, MessageService, HttpTestingController],
       schemas: [
         CUSTOM_ELEMENTS_SCHEMA,
         NO_ERRORS_SCHEMA
@@ -31,10 +31,99 @@ describe('CategoriaComponent', () => {
     });
     fixture = TestBed.createComponent(CategoriaComponent);
     component = fixture.componentInstance;
+    categoriaService = TestBed.inject(CategoriaService);
+    httpMock = TestBed.inject(HttpTestingController);
+    messageService = TestBed.inject(MessageService);
+    table = { filterGlobal: jest.fn() } as unknown as Table;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'Categorias`, () => {
+    const fixture = TestBed.createComponent(CategoriaComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Categorias');
+  });
+
+  it('should return the maximum id and sum 1', () => {
+    component.categoriaTable = [
+      { id: 1, name: 'Teste', status: 'Ativa' },
+      { id: 2, name: 'Teste', status: 'Ativa' },
+      { id: 3, name: 'Teste', status: 'Ativa' }
+    ]
+    const maxId = component.getMaxIdProdutosAndSumOne();
+    expect(maxId).toBe(4);
+  });
+
+  it('should open New', () => {
+    component.openNew();
+    expect(component.openNew).toBeTruthy();
+  });
+
+  it('should hideDialog', () => {
+    component.hideDialog();
+    expect(component.hideDialog).toBeTruthy();
+  });
+
+  it('should showDialog', () => {
+    component.showDialog();
+    expect(component.showDialog).toBeTruthy();
+  });
+
+  it('should load categorias', () => {
+    const mockCategorias: ICategoria[] = [
+      { id: 1, name: 'Teste', status: 'Ativa' }
+    ];
+    jest.spyOn(categoriaService, 'loadCategorias').mockReturnValue(of(mockCategorias))
+    component.loadCategorias();
+    expect(component.categoriaTable).toEqual(mockCategorias);
+    expect(categoriaService.loadCategorias).toHaveBeenCalled();
+  });
+
+  it('should save the categoria', () => {
+    const categoria = { id: 1, name: 'Teste', status: 'Ativa' } as ICategoria;
+    component.categoria = categoria;
+    jest.spyOn(categoriaService, 'saveCategorias').mockReturnValue(of(categoria))
+    component.saveCategoria();
+    expect(categoriaService.saveCategorias).toHaveBeenCalledWith(categoria);
+    expect(component.categoriaTable).toContain(categoria);
+  });
+
+  it('should delete the categoria', () => {
+    const mockCategoria = { id: 1, name: 'Teste', status: 'Ativa' };
+    component.categoriaSelected = mockCategoria;
+    jest.spyOn(categoriaService, 'deleteCategorias').mockReturnValue(of(mockCategoria))
+    component.confirmDelete();
+    expect(categoriaService.deleteCategorias).toHaveBeenCalledWith(mockCategoria.id);
+    expect(component.categoriaTable).not.toContain(mockCategoria);
+  });
+
+  it('should edit the categoria', () => {
+    const editedCategoria = { id: 1, name: 'Teste', status: 'Cancelada' };
+    jest.spyOn(categoriaService, 'editCategorias').mockReturnValue(of(editedCategoria));
+
+    component.categoriaTable = [
+      { id: 1, name: 'Teste', status: 'Ativa' },
+      { id: 2, name: 'Teste', status: 'Ativa' }
+    ];
+    component.categoriaSelected = { id: 1, name: 'Teste', status: 'Ativa' };
+
+    component.editCategoria(editedCategoria);
+
+    expect(categoriaService.editCategorias).toHaveBeenCalledWith(editedCategoria);
+    expect(component.categoriaTable).toEqual([
+      editedCategoria,
+      { id: 2, name: 'Teste', status: 'Ativa' }
+    ]);
+  });
+
+  it('should set categoriaEditDialog to be true and update categoriaSelected', () => {
+    const categoria: ICategoria = { id: 1, name: 'Teste', status: 'Ativa' };
+    component.selectEditCategoria(categoria);
+    expect(component.categoriaEditDialog).toBe(true);
+    expect(component.categoriaSelected).toEqual(categoria);
   });
 });
--- src/app/components/home/produto/produto.component.spec.ts
+++ src/app/components/home/produto/produto.component.spec.ts
@@ -1,25 +1,157 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
-import { ProdutoComponent } from './produto.component';
 import { MessageService } from 'primeng/api';
-import { HttpClientModule } from '@angular/common/http';
+import { ProdutoComponent } from './produto.component';
+import { ProdutoService } from '../../../services/produto.service';
+import { CategoriaService } from '../../../services/categoria.service';
+import { IProduto } from 'src/app/interfaces/iProduto';
+import { ICategoria } from 'src/app/interfaces/iCategoria';
 
 describe('ProdutoComponent', () => {
   let component: ProdutoComponent;
   let fixture: ComponentFixture<ProdutoComponent>;
+  let produtoService: ProdutoService;
+  let categoriaService: CategoriaService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ProdutoComponent],
       imports: [HttpClientModule],
-      providers: [MessageService]
+      providers: [HttpClient, MessageService, HttpTestingController],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA
+      ]
+
     });
     fixture = TestBed.createComponent(ProdutoComponent);
     component = fixture.componentInstance;
+    categoriaService = TestBed.inject(CategoriaService);
+    produtoService = TestBed.inject(ProdutoService);
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'Produtos`, () => {
+    const fixture = TestBed.createComponent(ProdutoComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Produtos');
+  });
+
+  it('should return the maximum id and sum 1', () => {
+    component.produtoTable = [
+      { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' },
+      { id: 2, name: 'Teste', categoria: 'Teste', preco: 200, quantidade: 20, descricao: 'Teste' },
+      { id: 3, name: 'Teste', categoria: 'Teste', preco: 300, quantidade: 30, descricao: 'Teste' }
+    ]
+    const maxId = component.getMaxIdProdutosAndSumOne();
+    expect(maxId).toBe(4);
+  });
+
+  it('should load produtos', () => {
+    const mockProdutos: IProduto[] = [
+      { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' },
+      { id: 2, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' }
+    ]
+
+    jest.spyOn(produtoService, 'loadProdutos').mockReturnValue(of(mockProdutos))
+
+    component.loadProdutos();
+
+    expect(component.produtoTable).toEqual(mockProdutos);
+    expect(produtoService.loadProdutos).toHaveBeenCalled();
+  });
+
+  it('should load categorias', () => {
+    const mockCategorias: ICategoria[] = [
+      { id: 1, name: 'Teste', status: 'Ativa' },
+      { id: 2, name: 'Teste', status: 'Ativa' }
+    ];
+    jest.spyOn(categoriaService, 'loadCategorias').mockReturnValue(of(mockCategorias))
+    component.loadCategorias();
+    expect(component.categorias).toEqual(mockCategorias);
+    expect(categoriaService.loadCategorias).toHaveBeenCalled();
+  });
+
+  it('should save the produto', () => {
+    const produto = { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' } as IProduto;
+    component.produto = produto;
+    jest.spyOn(produtoService, 'saveProdutos').mockReturnValue(of(produto))
+    component.saveProduto()
+    expect(produtoService.saveProdutos).toHaveBeenCalledWith(produto);
+    expect(component.produtoTable).toContain(produto);
+  });
+
+  it('should delete the produto', () => {
+    const mockProduto = { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' };
+    component.produtoSelected = mockProduto;
+    jest.spyOn(produtoService, 'deleteProdutos').mockReturnValue(of(mockProduto))
+    component.confirmDelete();
+    expect(produtoService.deleteProdutos).toHaveBeenCalledWith(mockProduto.id);
+    expect(component.produtoTable).not.toContain(mockProduto);
+  });
+
+  it('should edit the produto', () => {
+    const editedProduto = { id: 1, name: 'Teste', categoria: '', preco: 150, quantidade: 15, descricao: 'Teste' };
+    jest.spyOn(produtoService, 'editProdutos').mockReturnValue(of(editedProduto));
+
+    component.produtoTable = [
+      { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' },
+      { id: 2, name: 'Teste', categoria: 'Teste', preco: 200, quantidade: 20, descricao: 'Teste' }
+    ];
+    component.produtoSelected = { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' };
+
+    component.editProduto(editedProduto);
+
+    expect(produtoService.editProdutos).toHaveBeenCalledWith(editedProduto);
+    expect(component.produtoTable).toEqual([
+      editedProduto,
+      { id: 2, name: 'Teste', categoria: 'Teste', preco: 200, quantidade: 20, descricao: 'Teste' }
+    ]);
+  });
+
+  it('should clear the produto', () => {
+    component.visibleNewProduto = true;
+    component.produtoEditDialog = true;
+    component.produto = { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' } as IProduto;
+    component.categoriaSelected = 'Teste';
+
+    component.clearProduto();
+
+    expect(component.visibleNewProduto).toBe(false);
+    expect(component.produtoEditDialog).toBe(false);
+    expect(component.produto).toEqual({} as IProduto);
+    expect(component.categoriaSelected).toBe('');
+  });
+
+  it('should set produtoEditDialog', () => {
+    const produto: IProduto = { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' };
+    component.selectEditProduto(produto);
+    expect(component.produtoEditDialog).toBe(true);
+    expect(component.produtoSelected).toEqual(produto);
+  });
+
+  it('should set produtoDeleteDialog', () => {
+    const produto: IProduto = { id: 1, name: 'Teste', categoria: 'Teste', preco: 100, quantidade: 10, descricao: 'Teste' };
+    component.selectDeleteProduto(produto);
+    expect(component.deleteProdutoDialog).toBe(true);
+    expect(component.produtoSelected).toEqual(produto);
+  });
+
+  it('should set visibleNewProduto', () => {
+    component.visibleNewProduto = false;
+    component.categoriaSelected = 'Teste';
+    component.openNewProduto();
+    expect(component.visibleNewProduto).toBe(true);
+    expect(component.categoriaSelected).toEqual('');
   });
 });
